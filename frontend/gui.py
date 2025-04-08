d08ae92fec58d9779ad5d5e6f403645b77604f9d
SirSkye <foo@email.com>
2025-04-08 12:04:00 -0400

    new gui

--- frontend/gui.py
+++ frontend/gui.py
@@ -9,106 +9,122 @@ class MainW(Tk):
         Tk.__init__(self, parent)
         self.parent = parent
         self.mainWidgets()
-        self.geometry("816x529")
+        self.geometry("890x600")
         self.configure(bg = "#FFFFFF")    
         self.resizable(False, False)
-        self.recorder = AudioToTextRecorder()
-        self.client = ollama.Client()
+        # self.recorder = AudioToTextRecorder()
+        # self.client = ollama.Client()
 
     def relative_to_assets(self, path: str) -> Path:
         OUTPUT_PATH = Path(__file__).parent
-        ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\aisha\Tkinter-Designer\output\build\assets\frame0")
+        ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\aisha\Tkinter-Designer\rachelsDesign\build\assets\frame0")
         return ASSETS_PATH / Path(path)
 
     def mainWidgets(self):
         canvas = Canvas(
             self,
             bg = "#FFFFFF",
-            height = 529,
-            width = 816,
+            height = 600,
+            width = 890,
             bd = 0,
             highlightthickness = 0,
-            relief = "ridge")
+            relief = "ridge"
+        )
+
         canvas.place(x = 0, y = 0)
-        entry_image_1 = PhotoImage(
-            file=self.relative_to_assets("entry_1.png"))
-        entry_bg_1 = canvas.create_image(
-            578.0,
-            160.5,
-            image=entry_image_1
-        )
-        entry_1 = Text(
-            bd=0,
-            bg="#D9D9D9",
-            fg="#000716",
-            highlightthickness=0
-        )
-        entry_1.place(
-            x=374.0,
-            y=47.0,
-            width=408.0,
-            height=225.0
-        )
-
         canvas.create_rectangle(
-            14.0,
-            265.0,
-            374.0,
-            512.0,
+            451.0,
+            178.0,
+            863.0,
+            569.0,
             fill="#D9D9D9",
             outline="")
 
         canvas.create_rectangle(
-            365.0,
-            8.0,
-            791.0,
-            38.0,
-            fill="#D9D9D9",
-            outline="")
-
-        canvas.create_text(
-            541.0,
-            16.0,
-            anchor="nw",
-            text="Study Material",
+            25.0,
+            178.0,
+            437.0,
+            569.0,
             fill="#000000",
-            font=("Inter", 12 * -1)
+            outline="")
+
+        canvas.create_rectangle(
+            37.0,
+            16.0,
+            853.0,
+            95.0,
+            fill="#000000",
+            outline="")
+
+        canvas.create_text(
+            375.0,
+            0.0,
+            anchor="nw",
+            text="Duck",
+            fill="#FFFFFF",
+            font=("Lohit Devanagari", 60 * -1)
         )
 
         canvas.create_text(
-            156.0,
-            250.0,
+            46.0,
+            193.0,
             anchor="nw",
             text="You Said....",
             fill="#000000",
             font=("Inter", 12 * -1)
         )
 
-        button_image_1 = PhotoImage(
+        canvas.create_rectangle(
+            33.0,
+            113.0,
+            346.0,
+            162.0,
+            fill="#000000",
+            outline="")
+
+        canvas.create_rectangle(
+            545.0,
+            113.0,
+            858.0,
+            162.0,
+            fill="#D9D9D9",
+            outline="")
+
+        canvas.create_text(
+            573.0,
+            113.0,
+            anchor="nw",
+            text="Duck Feedback",
+            fill="#000000",
+            font=("Lohit Devanagari", 30 * -1)
+        )
+
+        canvas.create_text(
+            47.0,
+            113.0,
+            anchor="nw",
+            text="Study Material",
+            fill="#FFFFFF",
+            font=("Lohit Devanagari", 30 * -1)
+        )
+
+        self.button_image_1 = PhotoImage(
             file=self.relative_to_assets("button_1.png"))
-        submitBtn = Button(
-            image=button_image_1,
+        button_1 = Button(
+            image=self.button_image_1,
             borderwidth=0,
             highlightthickness=0,
             command=lambda: print("button_1 clicked"),
             relief="flat"
         )
-        submitBtn.place(
-            x=443.0,
-            y=276.0,
-            width=306.0,
-            height=30.0
+        button_1.place(
+            x=360.0,
+            y=110.0,
+            width=59.0,
+            height=59.0
         )
-
-        canvas.create_text(
-            570.0,
-            283.0,
-            anchor="nw",
-            text="Submit",
-            fill="#000000",
-            font=("Inter", 12 * -1)
-        )
+        button_1.lift()
 
 gui = MainW(None)
-
+print("MAINLOPPP")
 gui.mainloop()
